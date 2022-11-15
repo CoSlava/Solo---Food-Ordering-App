c4d504ce26c987e66a2ed9fea8143d846f1cc8b3
CoSlava <vecojocari@gmail.com>
2022-11-15 14:06:54 +0200

    Commit Final Version

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 import {menuArray} from "/data.js"
-let orderDetails = document.getElementById("order-details")
-let thxPage = document.getElementById("thank-you")
+let orderArray = []
 let sum = 0
 
+let displayOrderDetails = document.getElementById("display-order-details")
+let addCardContainter = document.getElementById("add-card-container")
+let orderDetails = document.getElementById("order-details")
+let calculator = document.getElementById("calculator")
+let foodFeed = document.getElementById("list-items")
+let thxPage = document.getElementById("thank-you")
+
 menuArray.forEach(function(item){
-    document.getElementById("list-items").innerHTML += `
+    foodFeed.innerHTML += `
     <div class="product-list-container">
         <div class="emoji">
             ${item.emoji}
         </div>
-            <h3 class="item-name">${item.name}<div class="add-to-cart-btn" id="add-to-cart" data-add=${item.id}>+</div></h3>
+            <h3 class="item-name">${item.name}<div class="add-to-cart-btn" id="add-to-cart" "
+            data-add=${item.id}>+</div></h3>
             <p class="item-ingredients">${item.ingredients}</p>
             <h3 class="item-price">$${item.price}</h3>
         <div class="bottom-border"></div>
@@ -29,36 +36,51 @@ document.addEventListener("click", function(e){
 })
 
 function addToCartBtn(itemId){
-    const selectedItems = menuArray.filter(function(obj){
-        return obj.id == itemId
-    })[0]         
-        orderDetails.innerHTML += `
-            <div id="${selectedItems.id}">
-                <h3 class="item-name">${selectedItems.name}<span data-rmv=${selectedItems.id} class="remove-txt">remove</span><span class="item-price-cart">$${selectedItems.price}</span></h3>
-                </div>
-            </div>`   
-        document.getElementById("display-order-details").classList.remove("hidden")
-        sum += selectedItems.price
-        document.getElementById("calculator").innerHTML = `$${sum}`
+    const selectedItem = menuArray.filter(function(obj){
+       return obj.id == itemId
+    })[0]
+        orderArray.push(selectedItem)
+        renderCartItems()
+}
+
+function renderCartItems(){
+    orderDetails.innerHTML = ""
+    sum = 0
+        orderArray.forEach(function(item){
+            orderDetails.innerHTML += `
+                <div id="${item.id}">
+                    <h3 class="item-name">${item.name}<span data-rmv=${item.name} class="remove-txt">remove</span><span class="item-price-cart">$${item.price}</span></h3>
+                    </div>
+                </div>`
+            sum += item.price
+        })
+    
+    if (sum > 0) {
+        displayOrderDetails.classList.remove("hidden")
+    } else if (sum === 0) {
+        displayOrderDetails.classList.add("hidden")
+    }
+        calculator.innerHTML = `$${sum}`
         thxPage.classList.add("hidden")
 }
 
-function removeItemsFromCart(){
-    document.getElementById("display-order-details").classList.add("hidden")
-    document.getElementById("order-details").innerHTML = ""
-    sum = 0
+function removeItemsFromCart(itemName){
+    orderArray.splice(itemName, 1)
+    renderCartItems()
 }
 
 function completeOrderBtn(){
-    document.getElementById("add-card-container").classList.remove("hidden")
+    addCardContainter.classList.remove("hidden")
 }
 
 function processingThePayment(){
-    document.getElementById("add-card-container").classList.add("hidden")
-    document.getElementById("display-order-details").classList.add("hidden")
+    addCardContainter.classList.add("hidden")
+    displayOrderDetails.classList.add("hidden")
     thxPage.classList.remove("hidden")
     thxPage.innerHTML = `
     <div class="thanks-for-order">
         Thank you! Your order is on its way!
     </div>`
+    orderArray = []
+    sum = 0
 }
